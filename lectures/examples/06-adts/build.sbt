983a5a616876ddc8e0ae36bbfaaa9ce0894a4b8f
Stoyan <stoyan.mihaylov@abv.bg>
2026-03-25 00:06:59 +0200

    Add adts slides and examples

---
+++ lectures/examples/06-adts/build.sbt
@@ -0,0 +1,14 @@
+name := "folds-and-collections"
+version := "0.1"
+
+scalaVersion := "3.8.2"
+
+libraryDependencies ++= Seq(
+  "org.scalatest" %% "scalatest" % "3.2.19" % Test
+)
+
+scalacOptions ++= Seq(
+  "-new-syntax",
+  "-Werror",
+  "-deprecation"
+)
---
+++ lectures/examples/06-adts/src/main/scala/scalafmi/CoffeeRobot.scala
@@ -0,0 +1,38 @@
+package scalafmi
+
+// TODO: Refactor Coffee Robot to eliminate illegal states
+// We first need to answer the question: what are the illegal states we want to eliminate and why?
+
+// no need to implement prepareIrishCoffee or methods for other coffee types
+
+class CoffeeRobot:
+  type Milk = String
+  type Gelato = String
+  type Cream = String
+  type Whiskey = String
+
+  enum CoffeeType:
+    case Cappuccino, Espresso, Latte, Affogato, IrishCoffee
+
+  case class CoffeeOrder(
+    coffeeType: CoffeeType,
+    milk: Option[Milk],
+    gelato: Option[Gelato],
+    cream: Option[Cream],
+    whiskey: Option[Whiskey]
+  )
+
+  def prepareIrishCoffee(cream: Cream, whiskey: Whiskey): Unit = ???
+
+  def prepareCoffeeOrder(order: CoffeeOrder): Unit =
+    order.coffeeType match
+      case CoffeeType.IrishCoffee =>
+        //Unreachable state but check just in case
+        if order.cream.isEmpty then throw IllegalStateException("No cream")
+        if order.whiskey.isEmpty then throw IllegalStateException("No whiskey")
+
+        prepareIrishCoffee(order.cream.get, order.whiskey.get)
+      case _ => ()
+
+
+
